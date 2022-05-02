659cb10c946df803807e167b8bfb8e727f94f72f
oldbi <oldbig71@yahoo.com.tw>
2022-05-02 16:16:11 +0800

    add filter

--- read.py
+++ read.py
@@ -3,9 +3,11 @@ with open('reviews.txt', 'r') as f: # C:/Users/oldbi/iCloud/test/review.py
     for line in f:
         data.append(line)
 
-# print(len(data))
 
-# print(data)
-print(data[0])
-print('---------------------')
-print(data[1])
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), ' 筆留言長度小於100')
+print(new[0])
+
